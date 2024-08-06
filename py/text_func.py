abac3dde390d1ded3d00344228628ec0c6dc4d19
syaofox <syaofox@gmail.com>
2024-08-06 21:27:32 +0800

    添加功能

--- py/text_func.py
+++ py/text_func.py
@@ -1,4 +1,5 @@
 import re
+from pathlib import Path
 
 class RegTextFind:
     def __init__(self):
@@ -32,13 +33,54 @@ class RegTextFind:
             print("No match found")
 
         return (result,)
+    
+
+class GenSwapPathText:
+    dir_dict = {}
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        target_dir =r"D:\adult\__face_pices\group"
+        for d in  Path(target_dir).iterdir():
+            if d.is_dir():
+                cls.dir_dict[d.name] = d
+        
+
+
+        return {
+            "required": {
+                "face_name": (list(cls.dir_dict.keys()),),    
+                "pices_path":  ("STRING", {"default": '', "multiline": True}),
+            }
+        }
+
+    RETURN_TYPES = ("STRING", "STRING", "STRING")
+    RETURN_NAMES = ("face_path", "pices_path", "saves_path",)
+    FUNCTION = "execute"
+
+    CATEGORY = "FoxTools/Text"
+
+    def execute(self, face_name, pices_path):
+
+ 
+        pices_driname = Path(pices_path).name
+        saves_path = rf'{face_name}\{pices_driname}\swap_'
+
+        return (str(self.dir_dict[face_name]), pices_path, saves_path,)
+    
+
 
 NODE_CLASS_MAPPINGS = {
-    "Foxtools: RegTextFind": RegTextFind
+    "Foxtools: RegTextFind": RegTextFind,
+    "Foxtools: GenSwapPathText": GenSwapPathText
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Foxtools: RegTextFind": "Foxtools: RegTextFind"
+    "Foxtools: RegTextFind": "Foxtools: RegTextFind",
+    "Foxtools: GenSwapPathText": "Foxtools: GenSwapPathText"
 }
 
 
